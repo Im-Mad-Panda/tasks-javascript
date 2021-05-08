2b5aa7b348fc9409a1533218375c695a7ebefcfa
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-05-08 23:37:29 +0300

    task

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// 2.4 task 1
+{
+  let admin, name;
+  name = "Джон";
+  admin = name;
+  alert(admin);
+}
+// 2.4 task 2
+{
+  let planetName = "Земля";
+  let userName = "user1";
+}
+// 2.4 task 3
+{
+  const BIRTHDAY = "18.04.1982";
+  const AGE = someCode(BIRTHDAY);
+}
+// 2.5 task 1
+{
+  let name = "Ilya";
+  alert(`hello ${1}`);
+  alert(`hello ${"name"}`);
+  alert(`hello ${name}`);
+}
+// 2.8 task 1
+{
+  let a = 1,
+    b = 1;
+  alert(++a);
+  alert(b++);
+  alert(a);
+  alert(b);
+}
+{
+  // 2.8 task 2
+  let a = 2;
+  console.log(a);
+  let x = 1 + (a *= 2);
+  console.log(x);
+}
