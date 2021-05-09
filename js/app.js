ad5ec24f2275c6941de33bc6404a21dfa0520f77
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-05-09 11:43:28 +0300

    task4

--- js/app.js
+++ js/app.js
@@ -43,19 +43,25 @@
 }
 // 2.8 task 3
 {
-console.log("" + 1 + 0)
-console.log("" - 1 + 0)
-console.log(true + false)
-console.log(6 / "3")
-console.log("2" * "3")
-console.log(4 + 5 + "px")
-console.log("$" + 4 + 5)
-console.log("4" - 2)
-console.log("4px" - 2)
-console.log(7 / 0)
-console.log("  -9  " + 5)
-console.log("  -9  " - 5)
-console.log(null + 1)
-console.log(undefined + 1)
-console.log(" \t \n" - 2)
+  console.log("" + 1 + 0);
+  console.log("" - 1 + 0);
+  console.log(true + false);
+  console.log(6 / "3");
+  console.log("2" * "3");
+  console.log(4 + 5 + "px");
+  console.log("$" + 4 + 5);
+  console.log("4" - 2);
+  console.log("4px" - 2);
+  console.log(7 / 0);
+  console.log("  -9  " + 5);
+  console.log("  -9  " - 5);
+  console.log(null + 1);
+  console.log(undefined + 1);
+  console.log(" \t \n" - 2);
+}
+// 2.8 task 4
+{
+  let a = prompt("Первое число?", 1);
+  let b = prompt("Второе число?", 2);
+  console.log(Number(a) + Number(b));
 }
